7060b93e4ad22c335c69e3f61e4bbc50e1811ea3
HelinXu <xuhelin1911@gmail.com>
2022-09-15 22:07:04 +0800

    [add] drawer6 categorize

--- data/0602_urdf_rootpose_correction.py
+++ data/0602_urdf_rootpose_correction.py
@@ -1,7 +1,7 @@
 '''
 Author: HelinXu xuhelin1911@gmail.com
 Date: 2022-09-15 16:16:55
-LastEditTime: 2022-09-15 22:04:02
+LastEditTime: 2022-09-15 22:05:52
 Description: 
 '''
 from glob import glob
@@ -199,6 +199,26 @@ if __name__ == '__main__':
         new_joint.append(ET.Element('parent', link='root'))
         new_joint.append(ET.Element('child', link='base_link'))
         new_joint.append(ET.Element('origin', xyz='0 0 0', rpy='3.14159265359 1.57079633 3.14159265359'))
+        node.append(new_joint)
+        # save the etree
+        with open(file.replace('sapien.urdf', 'sapien_v2.urdf'), 'wb') as f:
+            f.write(ET.tostring(node, pretty_print=True))
+
+    # drawer6
+    for i in range(len(CAT['drawer'][6]['ids'])):
+        cat = 'drawer'
+        idx = CAT[cat][6]["ids"][i]
+        file = f'processed/{cat}/{idx}/motion_sapien.urdf'
+        node, path = load_urdf(file)
+        ic(file)
+        # add a new link
+        new_link = ET.Element('link', name='root')
+        node.append(new_link)
+        # add a new joint
+        new_joint = ET.Element('joint', name='root_joint', type='fixed')
+        new_joint.append(ET.Element('parent', link='root'))
+        new_joint.append(ET.Element('child', link='base_link'))
+        new_joint.append(ET.Element('origin', xyz='0 0 0', rpy='3.14159265359 3.14159265359 0'))
         node.append(new_joint)
         # save the etree
         with open(file.replace('sapien.urdf', 'sapien_v2.urdf'), 'wb') as f:
